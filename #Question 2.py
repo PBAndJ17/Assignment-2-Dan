55984e01865f839c72d503769445661aee5e0d74
PBAndJ17 <lamsalkrish17@gmail.com>
2026-01-13 01:04:51 +0930

    Update #Question 2.py

--- #Question 2.py
+++ #Question 2.py
@@ -36,3 +36,39 @@ depth = int(input("Enter the recursion depth: "))
 draw_polygon(sides, length, depth)
 
 turtle.done()
+def encrypt_text(text, key):
+    encrypted_text = ""
+    for char in text:
+        encrypted_text += chr(ord(char) + key)
+    return encrypted_text
+
+
+def decrypt_text(text, key):
+    decrypted_text = ""
+    for char in text:
+        decrypted_text += chr(ord(char) - key)
+    return decrypted_text
+
+
+def encrypt_file(filename, key):
+    with open(filename, "r+") as file:
+        content = file.read()
+        file.seek(0)
+        file.write(encrypt_text(content, key))
+        file.truncate()
+    print("Encrypted successfully")
+
+def decrypt_file(filename, key):
+    with open(filename, "r+") as file:
+        content = file.read()
+        file.seek(0)
+        file.write(decrypt_text(content, key))
+        file.truncate()
+    print("Decrypted successfully")
+
+filename = "raw_text.txt"
+
+if __name__ == "__main__":
+    decrypt_file(filename, 5)
+    
+print("hey")

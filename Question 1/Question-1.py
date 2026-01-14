edfb2d82f42386c3ba7cadd0d5fec9a9908073d9
PBAndJ17 <lamsalkrish17@gmail.com>
2026-01-14 17:16:23 +0930

    ;

--- Question 1/Question-1.py
+++ Question 1/Question-1.py
@@ -7,20 +7,23 @@ def shift_in_group(ch, shift, group):
 
 
 def encrypt_char(ch, shift1, shift2):
+    
     low1 = "abcdefghijklm"
     low2 = "nopqrstuvwxyz"
     up1  = "ABCDEFGHIJKLM"
     up2  = "NOPQRSTUVWXYZ"
 
+    
     if not ch.isalpha():
         return ch
 
+    
     k1 = shift1 * shift2      
     k2 = shift1 + shift2      
     k3 = shift1               
-    k4 = shift2 * shift2      
+    k4 = shift2 * shift2   
 
-
+    
     if ch in low1:
         return shift_in_group(ch, +k1, low1)
     elif ch in low2:
@@ -43,13 +46,11 @@ def decrypt_char(ch, shift1, shift2):
     if not ch.isalpha():
         return ch
 
-    
     k1 = shift1 * shift2
     k2 = shift1 + shift2
     k3 = shift1
     k4 = shift2 * shift2
 
-    
     if ch in low1:
         return shift_in_group(ch, -k1, low1)   # was +k1
     elif ch in low2:
@@ -80,25 +81,21 @@ def main():
     shift1 = int(input("Enter shift1 (integer): "))
     shift2 = int(input("Enter shift2 (integer): "))
 
-    
     with open("raw_text.txt", "r", encoding="utf-8") as f:
         raw = f.read()
 
-    
     encrypted = encrypt_text(raw, shift1, shift2)
     with open("encrypted_text.txt", "w", encoding="utf-8") as f:
         f.write(encrypted)
 
-    
     decrypted = decrypt_text(encrypted, shift1, shift2)
     with open("decrypted_text.txt", "w", encoding="utf-8") as f:
         f.write(decrypted)
 
-    
     if decrypted == raw:
-        print("SUCCESS: Decryption matches the original file.")
+        print("Decryption Successful.")
     else:
-        print("FAIL: Decryption does NOT match the original file.")
+        print("Decryption Failed.")
 
 
 main()
